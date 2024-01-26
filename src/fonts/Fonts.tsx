2dd5ba6b8a30e738fe2aa7c2bbf6f357646a6d7b
nirjhar sarkar <nirjharsarkar8@gmail.com>
2024-01-26 19:30:56 +0530

    removed some unnessesary font files

---
+++ src/fonts/Fonts.tsx
@@ -0,0 +1,6 @@
+"use client"
+import localFont from "next/font/local";
+
+export const bettynoir = localFont({ src: '../../public/fonts/bettynoir.ttf' })
+export const MinstrelPosterNF = localFont({ src: '../../public/fonts/MinstrelPosterNF.ttf' })
+export const Cocogoose = localFont({ src: '../../public/fonts/Cocogoose.ttf' })
