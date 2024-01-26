674782f94f2f32d1624c0d8b1117e035da2f01aa
nirjhar sarkar <nirjharsarkar8@gmail.com>
2024-01-26 19:43:09 +0530

    added smoothscroll to the app

---
+++ src/utils/Smoothscroll.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect } from "react";
+ //@ts-ignore
+import Lenis from "@studio-freight/lenis";
+
+interface props {
+  children: React.ReactNode;
+}
+
+const SmoothScroll: React.FC<props> = ({ children }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+  return children;
+};
+
+export default SmoothScroll;
